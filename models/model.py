f668415035c1dcca4876a690ae75d3b41d1f8915
yeppp27 <69355369+yeppp27@users.noreply.github.com>
2024-07-27 15:33:28 +0800

    Update model.py

--- models/model.py
+++ models/model.py
@@ -11,6 +11,7 @@ from .head import IQAHead, VARHead, VQAHead ,simpleVQAHead
 from models.backbones.swin_backbone import SwinTransformer2D as ImageBackbone
 from models.backbones.swin_backbone import SwinTransformer3D as VideoBackbone
 from models.backbones.simpleVQA_model import resnet50 as simpleVQA_Backbone
+from models.backbones.KSVQE_model import KSVQE as KSVQE_Backbone
 import torchvision.transforms as T
 import numpy as np 
         
@@ -52,6 +53,20 @@ class VQA_Network(nn.Module):
                 backbone = simpleVQA_Backbone(pretrained=True)
                 head = simpleVQAHead(**hypers['head'])
                 self.key_names.append(key)
+            elif key == "KSVQE":
+                                     
+                backbone =  KSVQE_Backbone(num_samples=hypers['backbone']['num_samples'],
+                                            sample_type=hypers['backbone']['sample_type'],
+                
+                                            CLIP_location=hypers['backbone']['CLIP_location'],
+                                            cls_use=hypers['backbone']['cls_use'],
+                                            tuning_stage = hypers['backbone']['tuning_stage'],
+                                            a1 = hypers['backbone']['a1'],
+                                            a2 = hypers['backbone']['a2'],
+                                            frozen_stages=hypers['backbone']['frozen_stages'],
+                                            )
+                head = VQAHead(**hypers['head'])
+                self.key_names.append(key)
             else:
                 raise NotImplementedError
 
@@ -74,10 +89,16 @@ class VQA_Network(nn.Module):
      
         scores = []
         feats = {}
+        dis_contra_loss = None
         for key in self.key_names:
-            feat = getattr(self, key + "_backbone")(
-                inputs, multi=self.multi, layer=self.layer, **kwargs
-            )
+            if key == 'KSVQE':
+                feat, dis_contra_loss = getattr(self, key + "_backbone")(
+                    inputs, multi=self.multi, layer=self.layer, **kwargs
+                )
+            else:
+                feat = getattr(self, key + "_backbone")(
+                    inputs, multi=self.multi, layer=self.layer, **kwargs
+                )
             scores += [getattr(self, key + "_head")(feat)]
             if return_pooled_feats:
                 feats[key] = feat
@@ -87,6 +108,14 @@ class VQA_Network(nn.Module):
             else:
                 scores = scores[0]
         
+        
         if return_pooled_feats:
-            return scores, feats
-        return scores
+            if dis_contra_loss is  not None:
+                 return scores, feats, dis_contra_loss
+            else:
+                 return scores, feats
+            
+        if dis_contra_loss is not None:
+            return scores, dis_contra_loss
+        else:
+            return scores
